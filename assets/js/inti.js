57631b30a0954486306e98617adc2cd7dd457ebc
abhuraya <149016492+abhuraya@users.noreply.github.com>
2024-10-17 21:54:10 -0400

    lsdjf

--- assets/js/inti.js
+++ assets/js/inti.js
@@ -37,7 +37,20 @@ window.addEventListener("load", function(){
                         })
                             .then((response) => response.text())
                             .then((response) => {
-                                console.log(response)
+                                const responseText = JSON.parse(response)
+                                if(responseText.error !== ""){
+                                    document.querySelector("#alert").
+                                    innerText = responseText.error
+                                    document.querySelector("#alert").
+                                    classList.add("error")
+                                    document.querySelector(".formfields").
+                                    style.display = "block"
+                                    return
+                                }
+                                document.querySelector("#alert").innerText = responseText.success
+                                    document.querySelector("#alert").
+                                    classList.add("success")
+
                             })
                     })
             })
