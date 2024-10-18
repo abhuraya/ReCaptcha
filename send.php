ef538d9e84294e77fc39fe0c2eb877c63d432970
abhuraya <149016492+abhuraya@users.noreply.github.com>
2024-10-17 21:46:39 -0400

    oasdf

--- send.php
+++ send.php
@@ -17,6 +17,18 @@ $error_output = '';
 
 
 if (isValid()) {
+    $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+    $recaptcha_secret = '6LddrmQqAAAAAEDaH01YmT8c4-c4CsGIqq4mQHgl';
+    $recaptcha_response = $_POST('recaptchaResponse')
+    $recaptcha = file_get_contents($recaptcha_url."?secre=t".
+    $recaptcha_secret.'&response='. $recaptcha_response);
+    $recaptcha = json_decode($recaptcha);
+    if($recaptcha->seccess == true && $recaptcha->score >= 0.5 && $recaptcha->action == "contact"){
+        //run email send routine
+        $success_output = 'Your message was sent successfully.';
+    }else{
+        $error_output = 'Something went wrong Please try again later';
+    }
     $success_output = "Your message was sent successfully.";
 }else{
     $error_output = 'Please fill out all the required fields.';
