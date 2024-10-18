b5125e5432bcbc04cdfce88a18222d253622e728
abhuraya <149016492+abhuraya@users.noreply.github.com>
2024-10-17 22:29:41 -0400

    jf

--- send.php
+++ send.php
@@ -19,7 +19,7 @@ $error_output = '';
 if (isValid()) {
     $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
     $recaptcha_secret = '6LddrmQqAAAAAEDaH01YmT8c4-c4CsGIqq4mQHgl';
-    $recaptcha_response = $_POST('recaptchaResponse')
+    $recaptcha_response = $_POST['recaptchaResponse'];
     $recaptcha = file_get_contents($recaptcha_url.'?secret='.
     $recaptcha_secret.'&response='. $recaptcha_response);
     $recaptcha = json_decode($recaptcha);
